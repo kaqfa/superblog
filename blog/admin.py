6a9a7b391354e3f45e2ea4ef79882bd7788cb4eb
Fahri Firdausillah <elfaatta@gmail.com>
2017-08-24 13:31:19 +0700

    komentar

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-# Register your models here.
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
 
@@ -12,4 +12,10 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
